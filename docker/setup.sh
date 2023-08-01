5da84c0091b09fbcbdee2b938aba310382686464
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-01 17:26:14 +0200

    changed tabs to spaces and limited line length

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,11 +6,13 @@ pkill Docker
 
 if [ -d "$docker_destination" ]
 then
-	read -n1 -p "Folder $docker_destination already exists, do you want to reset it? [y/N] " input
-	echo ""
-	if [ -n "$input" ] && [ "$input" = "y" ]; then
-		rm -rf "$docker_destination"/{com.docker.{docker,helper},.docker} &>/dev/null
-	fi
+  read -n1 -p "Folder $docker_destination already exists, \
+                do you want to reset it? [y/N] " input
+  echo ""
+  if [ -n "$input" ] && [ "$input" = "y" ]; then
+    rm -rf "$docker_destination"/{com.docker.{docker,helper},.docker} \
+        &>/dev/null
+  fi
 fi
 
 unlink ~/Library/Containers/com.docker.docker &>/dev/null
@@ -21,8 +23,10 @@ rm -rf ~/Library/Containers/com.docker.{docker,helper} ~/.docker &>/dev/null
 
 mkdir -p "$docker_destination"/{com.docker.{docker,helper},.docker}
 
-ln -sf "$docker_destination"/com.docker.docker ~/Library/Containers/com.docker.docker
-ln -sf "$docker_destination"/com.docker.helper ~/Library/Containers/com.docker.helper
+ln -sf "$docker_destination"/com.docker.docker \
+          ~/Library/Containers/com.docker.docker
+ln -sf "$docker_destination"/com.docker.helper \
+          ~/Library/Containers/com.docker.helper
 ln -sf "$docker_destination"/.docker ~/.docker
 
 echo "Docker now successfully is in goinfree"
