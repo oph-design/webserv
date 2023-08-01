ad5f972f503614d82abc21e29386d102dc9e02d5
Michael Graefen <michaelgraefen@gmail.com>
2023-08-01 16:37:14 +0200

    multi socket does compile

---
+++ resources/TCP_Test/tcp_test.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tcp_test.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgraefen <mgraefen@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 14:26:56 by mgraefen          #+#    #+#             */
+/*   Updated: 2023/08/01 14:28:14 by mgraefen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TCP_TEST_HPP
+#define TCP_TEST_HPP
+
+#include <netinet/in.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+#define MAX_CLIENTS 1024
+#endif
