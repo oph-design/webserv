4ef942756dc705e3e64a288da75a7984ff5930fb
oph-design <ole.paul.heinzelmann@gmail.com>
2023-08-02 11:33:37 +0200

    add response files

---
+++ src/core/Response.hpp
@@ -0,0 +1,21 @@
+#ifndef RESPONSE_HPP
+# define RESPONSE_HPP
+
+# include <netinet/in.h>
+# include <poll.h>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <cstring>
+# include <fstream>
+# include <iostream>
+# include <sstream>
+# include <string>
+# include <map>
+# include <algorithm>
+
+class Response {
+ public:
+ private:
+};
+
+#endif // !RESPONSEHPP
