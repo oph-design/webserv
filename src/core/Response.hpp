8824939d8f3fd1d3462d1f73ebb36596d69ae865
oph-design <ole.paul.heinzelmann@gmail.com>
2023-08-03 10:58:31 +0200

    add toString funciton to response class

--- src/core/Response.hpp
+++ src/core/Response.hpp
@@ -14,12 +14,16 @@
 #include <sstream>
 #include <string>
 
+typedef std::pair<std::string, std::string> contentField;
+typedef std::map<std::string, std::string>::iterator mapIterator;
+
 class Response {
  public:
   Response();
   Response(const Response& rhs);
   Response& operator=(const Response& rhs);
   ~Response();
+  std::string toString();
 
  private:
   std::string body_;
