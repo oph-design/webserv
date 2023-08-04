fbe1fd5fd59803b7d14d42d963508723f9d2ae8f
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-04 14:33:24 +0200

    added a few color definitions and a color reset definition

---
+++ include/colors.hpp
@@ -0,0 +1,15 @@
+#ifndef COLORS_HPP_
+#define COLORS_HPP_
+
+#define BLACK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define MAGENTA "\e[0;35m"
+#define CYAN "\e[0;36m"
+#define WHITE "\e[0;37m"
+
+#define COLOR_RESET "\e[0m"
+
+#endif  // COLORS_HPP_
