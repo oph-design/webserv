b7bada951b169cf66ee6dae71a4d6e7e6ff71643
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-04 14:40:46 +0200

    changed the definitions so they work on more platforms

--- include/colors.hpp
+++ include/colors.hpp
@@ -1,14 +1,14 @@
 #ifndef COLORS_HPP_
 #define COLORS_HPP_
 
-#define BLACK "\e[0;30m"
-#define RED "\e[0;31m"
-#define GREEN "\e[0;32m"
-#define YELLOW "\e[0;33m"
-#define BLUE "\e[0;34m"
-#define MAGENTA "\e[0;35m"
-#define CYAN "\e[0;36m"
-#define WHITE "\e[0;37m"
+#define BLACK "\33[0;30m"
+#define RED "\33[0;31m"
+#define GREEN "\33[0;32m"
+#define YELLOW "\33[0;33m"
+#define BLUE "\33[0;34m"
+#define MAGENTA "\33[0;35m"
+#define CYAN "\33[0;36m"
+#define WHITE "\33[0;37m"
 
 #define COLOR_RESET "\e[0m"
 
