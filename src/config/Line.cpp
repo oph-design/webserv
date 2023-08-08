4443ddf83f1a89fc0b6d6c76b439f61ee04b517e
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-08 13:05:53 +0200

    added basic line functionality

--- src/config/Line.cpp
+++ src/config/Line.cpp
@@ -1,12 +1,17 @@
 #include "Line.hpp"
 
-Line::Line() {}
+Line::Line() : lineNumber_(0), error_(false) {}
 
 Line::~Line() {}
 
 Line::Line(const Line& obj) { *this = obj; }
 
+Line::Line(int lineNumber, std::string content)
+    : lineNumber_(lineNumber), content_(content), error_(false) {}
+
 Line& Line::operator=(const Line& obj) {
-  (void)obj;
+  this->lineNumber_ = obj.lineNumber_;
+  this->content_ = obj.content_;
+  this->error_ = obj.error_;
   return *this;
 }
--- src/config/Line.hpp
+++ src/config/Line.hpp
@@ -1,14 +1,20 @@
 #ifndef LINE_HPP_
 #define LINE_HPP_
 
+#include <string>
+
 class Line {
  public:
   Line();
   ~Line();
   Line(const Line& obj);
+  Line(int lineNumber, std::string content);
   Line& operator=(const Line& obj);
 
  private:
+  int lineNumber_;
+  std::string content_;
+  bool error_;
 };
 
 #endif  // LINE_HPP_
