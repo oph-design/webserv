1dc935130e3dbcb89fbb63699ce343865a2f190e
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-10 13:40:36 +0200

    added this-> into socket class

--- src/core/Socket.cpp
+++ src/core/Socket.cpp
@@ -8,9 +8,9 @@ Socket::Socket()
       timestamp_(0),
       timeout_(5.0),
       inUse_(false) {
-  socketFd_.fd = -1;
-  socketFd_.events = POLLIN;
-  socketFd_.revents = 0;
+  this->socketFd_.fd = -1;
+  this->socketFd_.events = POLLIN;
+  this->socketFd_.revents = 0;
 }
 
 Socket::~Socket() {}
@@ -18,48 +18,48 @@ Socket::~Socket() {}
 Socket::Socket(const Socket &rhs) { *this = rhs; }
 
 Socket &Socket::operator=(const Socket &rhs) {
-  timestamp_ = rhs.timestamp_;
-  keepAlive_ = rhs.keepAlive_;
-  socketOpt_ = rhs.socketOpt_;
-  socketFd_ = rhs.socketFd_;
+  this->timestamp_ = rhs.timestamp_;
+  this->keepAlive_ = rhs.keepAlive_;
+  this->socketOpt_ = rhs.socketOpt_;
+  this->socketFd_ = rhs.socketFd_;
   return *this;
 }
 
 // getter
 
-int Socket::getRevents() const { return socketFd_.revents; }
+int Socket::getRevents() const { return this->socketFd_.revents; }
 
-struct pollfd Socket::getSocketPoll() const { return socketFd_; }
+struct pollfd Socket::getSocketPoll() const { return this->socketFd_; }
 
-bool Socket::getKeepAlive() const { return keepAlive_; }
+bool Socket::getKeepAlive() const { return this->keepAlive_; }
 
-int Socket::getSocketFd() { return socketFd_.fd; }
+int Socket::getSocketFd() { return this->socketFd_.fd; }
 
 // setter
 
-void Socket::setFd(int fd) { socketFd_.fd = fd; }
+void Socket::setFd(int fd) { this->socketFd_.fd = fd; }
 
 void Socket::setPollfd(const struct pollfd rhs) { this->socketFd_ = rhs; }
 
 void Socket::setTimestamp() { this->timestamp_ = std::time(NULL); }
 
-void Socket::setInUse(bool set) { inUse_ = set; }
+void Socket::setInUse(bool set) { this->inUse_ = set; }
 
-void Socket::setRevent(int revent) { socketFd_.revents = revent; }
+void Socket::setRevent(int revent) { this->socketFd_.revents = revent; }
 
-void Socket::setEvent(int event) { socketFd_.events = event; }
+void Socket::setEvent(int event) { this->socketFd_.events = event; }
 
-void Socket::setKeepAlive(bool set) { keepAlive_ = set; }
+void Socket::setKeepAlive(bool set) { this->keepAlive_ = set; }
 
 // other functions
 bool Socket::checkTimeout() {
   if (this->inUse_) {
     time_t current = std::time(NULL);
-    if (difftime(current, this->timestamp_) >= timeout_) return true;
+    if (difftime(current, this->timestamp_) >= this->timeout_) return true;
   }
   return false;
 }
 
-void Socket::closeSocket() { close(socketFd_.fd); }
+void Socket::closeSocket() { close(this->socketFd_.fd); }
 
 // private
