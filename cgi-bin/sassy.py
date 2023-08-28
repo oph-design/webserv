ff5835f4a27c59ff32589a08a8a57a400d6c6e74
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-28 11:25:52 +0200

    removed some tabs

--- cgi-bin/sassy.py
+++ cgi-bin/sassy.py
@@ -8,24 +8,24 @@ from random import choice
 comment = [
     'Nice individual linter u got there',
     'You know we were supposed to write c++ code rigth? But maybe it compiles anyway. \n\
-      \n\
-      On a serious note:\n\
-      It`s a great summary and is in accordance with what I read so far.\n\
-      Only thing I`d add would be examples for request-, response headers,etc.\n\
-      But we can do that later, enjoy your sunday! ^^',
+\n\
+On a serious note:\n\
+It`s a great summary and is in accordance with what I read so far.\n\
+Only thing I`d add would be examples for request-, response headers,etc.\n\
+But we can do that later, enjoy your sunday! ^^',
     '5 failed pushes? \n\
-      Learn how to code inside the norm',
+Learn how to code inside the norm',
     'The code looks ok',
     'Ok ...\n\
-      random ass pull request, but Ok',
+random ass pull request, but Ok',
     'What a wonderful contribution\n\
-      I`m sure we`re not going to wait to use it for the next 3 weeks',
+I`m sure we`re not going to wait to use it for the next 3 weeks',
     'LGTM',
     'changed a file unrelated to the topic of the PR.\n\
-      If you weren`t a junior you would be fired now.',
+If you weren`t a junior you would be fired now.',
     'I guess because you couldn`t get through our code styling requirements you had to change them. Very smart ... very smart',
     'ok, but next time pls not decide something like that on the fly.\n\
-      Instead setup a meeting in 3 days with at least 2 follow up meetings and 3 unnecessary attendants to make the decision',
+Instead setup a meeting in 3 days with at least 2 follow up meetings and 3 unnecessary attendants to make the decision',
     'JA GANZ TOLL!',
     'Bro what`s wrong with this test case',
     'These changes will boost our performance by at least 0,01 micro seconds per iteration in each loop. Mind Blowing 🤯',
