8bc704533a8397fb7da4aff296a59ba573652c51
Florian Sandel <fsandel@student.42heilbronn.de>
2023-08-29 15:36:55 +0200

    added missing undescore

--- src/config/ConfigFile.hpp
+++ src/config/ConfigFile.hpp
@@ -35,7 +35,7 @@ class ConfigFile {
   friend std::ostream& operator<<(std::ostream& stream, ConfigFile& config);
 
  private:
-  static ConfigVector splitUpListens(ConfigVector& configvector);
+  static ConfigVector splitUpListens_(ConfigVector& configvector);
   void cleanContent_();
   void vaildateConfigFile_();
   ConfigVector createConfigVector_();
