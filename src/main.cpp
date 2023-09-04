67c920179424b6325a790aa49be40caee9105146
Florian Sandel <fsandel@student.42heilbronn.de>
2023-09-04 16:26:20 +0200

    moved signal catcher into his own function

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "ConfigFile.hpp"
 #include "Webserver.hpp"
 
+void signalHandling();
+
 int main(int argc, char *argv[]) {
-#ifdef __linux__
-  signal(SIGPIPE, SIG_IGN);
-#endif
+  signalHandling();
   ConfigFile configFile;
   if (!configFile.openFile(argc, argv)) return EXIT_FAILURE;
   ConfigVector configs = configFile.createConfig();
@@ -17,4 +17,10 @@ int main(int argc, char *argv[]) {
     Webserver Server(configs);
   }
   return EXIT_SUCCESS;
+}
+
+void signalHandling() {
+#ifdef __linux__
+  signal(SIGPIPE, SIG_IGN);
+#endif
 }
