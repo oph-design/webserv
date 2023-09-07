9ad1c8a9824716d76363314b1aabed785f2fd40f
Florian Sandel <fsandel@student.42heilbronn.de>
2023-09-07 13:28:19 +0200

    adde rewrite member variable

--- src/config/LocationGet.cpp
+++ src/config/LocationGet.cpp
@@ -1,5 +1,7 @@
 #include "Location.hpp"
 
+const std::string& Location::getRewrite() const { return this->rewrite_; }
+
 const bool& Location::getAutoindex() const { return this->autoindex_; }
 
 const int& Location::getClientMaxBodySize() const {
