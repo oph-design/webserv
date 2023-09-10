54aebbcf9d6862064adc4c58c35748acf0eb19eb
Florian Sandel <florian@sandel.email>
2023-09-10 19:21:56 +0200

    clang-format

--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -7,21 +7,20 @@ contentMap Response::fileTypes_ = Response::createTypeMap();
 Response::Response(Request &request, const Location &location)
     : location_(location) {
   printVerbose(MAGENTA, this->location_);
-  if (this->redirect())
-    return;
+  if (this->redirect()) return;
   switch (request.getRequestMethodType()) {
-  case 0:
-    handleGetRequest_(request, request.cutPath(location_.getPath()));
-    break;
-  case 1:
-    handlePostRequest_(request, request.cutPath(location_.getPath()));
-    break;
-  case 2:
-    handleDeleteRequest_(request, request.cutPath(location_.getPath()));
-    break;
-  default:
-    this->status_ = 501;
-    this->status_ >> this->body_;
+    case 0:
+      handleGetRequest_(request, request.cutPath(location_.getPath()));
+      break;
+    case 1:
+      handlePostRequest_(request, request.cutPath(location_.getPath()));
+      break;
+    case 2:
+      handleDeleteRequest_(request, request.cutPath(location_.getPath()));
+      break;
+    default:
+      this->status_ = 501;
+      this->status_ >> this->body_;
   }
 }
 
@@ -88,11 +87,9 @@ bool Response::isForbiddenPath_(const std::string &dir) {
   int counter = 0;
   std::string buffer;
   while (std::getline(ss, buffer, '/')) {
-    if (buffer.empty())
-      continue;
+    if (buffer.empty()) continue;
     (buffer == "..") ? ++counter : --counter;
-    if (counter > 0)
-      return true;
+    if (counter > 0) return true;
   }
   return false;
 }
@@ -113,8 +110,7 @@ void Response::readBody_(const std::string &dir) {
 
 void Response::handleGetRequest_(Request &request, const std::string &uri) {
   std::string path = location_.getRoot() + uri;
-  if (!this->prerequisites_("POST", request))
-    return;
+  if (!this->prerequisites_("POST", request)) return;
   if (CgiConnector::isCgi(location_.getCgiPass() + uri))
     return (void)(serveCgi_(request));
   if (Response::isFolder_(path) && !this->location_.getAutoindex())
@@ -122,10 +118,8 @@ void Response::handleGetRequest_(Request &request, const std::string &uri) {
   else if (Response::isFolder_(path) && !this->location_.getAutoindex())
     return (void)(serveFolder_(path));
 
-  if (this->status_ == 200)
-    readBody_(path);
-  if (this->status_ == 200)
-    findType_(path);
+  if (this->status_ == 200) readBody_(path);
+  if (this->status_ == 200) findType_(path);
   if (this->status_ > 399) {
     this->status_ >> this->body_;
     this->type_ = "text/html; charset=UTF-8";
@@ -145,11 +139,9 @@ void Response::createFile_(const std::string &filename, const std::string &ext,
   std::string path = this->location_.getUploadPass();
   path = path + "/" + filename + "." + ext;
   this->status_ = 201;
-  if (!access(path.c_str(), F_OK))
-    this->status_ = 200;
+  if (!access(path.c_str(), F_OK)) this->status_ = 200;
   std::ofstream outfile((path).c_str());
-  if (!outfile.is_open())
-    this->status_ = 403;
+  if (!outfile.is_open()) this->status_ = 403;
   outfile.write(data.c_str(), data.length());
   outfile.close();
 }
@@ -168,8 +160,7 @@ void Response::buildJsonBody_() {
 
 void Response::handlePostRequest_(const Request &request,
                                   const std::string &uri) {
-  if (!this->prerequisites_("POST", request))
-    return;
+  if (!this->prerequisites_("POST", request)) return;
   if (CgiConnector::isCgi(location_.getCgiPass() + uri))
     return (void)(serveCgi_(request));
   std::string file = request.getPath();
@@ -197,14 +188,11 @@ void Response::handlePostRequest_(const Request &request,
 void Response::handleDeleteRequest_(const Request &request,
                                     const std::string &uri) {
   std::string path = location_.getRoot() + uri;
-  if (!this->prerequisites_("DELETE", request))
-    return;
+  if (!this->prerequisites_("DELETE", request)) return;
   if (CgiConnector::isCgi(this->location_.getCgiPass() + uri))
     return (void)(serveCgi_(request));
-  if (access(path.c_str(), F_OK))
-    this->status_ = 403;
-  if (this->status_ < 400 && remove(path.c_str()))
-    this->status_ = 500;
+  if (access(path.c_str(), F_OK)) this->status_ = 403;
+  if (this->status_ < 400 && remove(path.c_str())) this->status_ = 500;
   this->header_.insert(contentField("Connection", "keep-alive"));
   this->header_.insert(contentField("Content-Type", "application/json"));
   buildJsonBody_();
@@ -218,8 +206,7 @@ bool Response::prerequisites_(const std::string &method,
     this->status_ = 405;
   else if (this->location_.maxBodyReached(request.getRequestBody().size()))
     this->status_ = 413;
-  if (this->status_ < 400)
-    return (true);
+  if (this->status_ < 400) return (true);
   this->status_ >> this->body_;
   std::string length = toString<std::size_t>(this->body_.length());
   this->type_ = "text/html; charset=UTF-8";
@@ -248,8 +235,7 @@ void Response::serveCgi_(const Request &request) {
 /*                Folder Request                  */
 void Response::serveFolder_(const std::string &path) {
   bool autoindex = true;
-  if (autoindex)
-    this->body_ = createFolderBody_(path);
+  if (autoindex) this->body_ = createFolderBody_(path);
   this->header_.insert(contentField("Content-Type", "text/html"));
   this->header_.insert(contentField("Connection", "keep-alive"));
   this->header_.insert(
@@ -264,14 +250,11 @@ std::deque<std::string> Response::getFilesInFolder_(const std::string &path) {
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
       std::string fileName = entry->d_name;
-      if (fileName == "." || (folderPath == "/" && fileName == ".."))
-        continue;
+      if (fileName == "." || (folderPath == "/" && fileName == "..")) continue;
       struct stat fileStat = {};
-      if (folderPath[folderPath.length() - 1] != '/')
-        fileName = '/' + fileName;
+      if (folderPath[folderPath.length() - 1] != '/') fileName = '/' + fileName;
       const std::string &filePath(path);
-      if (stat(filePath.c_str(), &fileStat) == 0)
-        names.push_back(fileName);
+      if (stat(filePath.c_str(), &fileStat) == 0) names.push_back(fileName);
     }
     closedir(dir);
   }
@@ -287,8 +270,7 @@ std::string Response::createFolderBody_(const std::string &path) {
   body.append("<head><title>Index of" + uri + " </title></head>\n");
   body.append("<body>\n");
   body.append("<h1>Index of " + uri + " </h1><hr><pre>\n");
-  if (last(uri) == '/')
-    uri = uri.substr(0, uri.size() - 1);
+  if (last(uri) == '/') uri = uri.substr(0, uri.size() - 1);
   for (std::deque<std::string>::iterator iter = names.begin();
        iter != names.end(); ++iter) {
     body.append("<a href=\"" + uri + *iter + "\">");
@@ -313,8 +295,7 @@ bool Response::isFolder_(const std::string &uri) {
 }
 
 bool Response::redirect() {
-  if (this->location_.getRewrite().empty())
-    return (false);
+  if (this->location_.getRewrite().empty()) return (false);
   this->status_ = 301;
   this->header_.insert(contentField("Location", this->location_.getRewrite()));
   this->header_.insert(contentField("Connection", "Keep-alive"));
